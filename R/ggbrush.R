b63a907633265be64a1bf188c11be8ca442459a2
unknown <david.elias949@gmail.com>
2022-01-30 00:37:29 -0300

    My first addin

---
+++ R/ggbrush.R
@@ -0,0 +1,31 @@
+
+
+
+ggbrush <- function(data = mtcars, xvar = "mpg", yvar = "disp") {
+
+  ui <- miniPage(
+    gadgetTitleBar("Drag to select points"),
+    miniContentPanel(
+      # The brush="brush" argument means we can listen for
+      # brush events on the plot using input$brush.
+      plotOutput("plot", height = "100%", brush = "brush")
+    )
+  )
+
+  server <- function(input, output, session) {
+
+    # Render the plot
+    output$plot <- renderPlot({
+      # Plot the data with x/y vars indicated by the caller.
+      ggplot(data, aes_string(xvar, yvar)) + geom_point()
+    })
+
+    # Handle the Done button being pressed.
+    observeEvent(input$done, {
+      # Return the brushed points. See ?shiny::brushedPoints.
+      stopApp(brushedPoints(data, input$brush))
+    })
+  }
+
+  runGadget(ui, server)
+}
